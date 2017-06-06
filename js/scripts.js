1679ab6fdb525e75142dc7538c1d486a93e0506b
jacob ruleaux and Michelle Poterek <jacob@>
2017-06-06 13:42:34 -0700

    learn that for loops require square brackets when setting conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-  $("").submit(function(event) {
+  $("#userInput").submit(function(event) {
     event.preventDefault();
+    var words = $("#sentence").val().toLowerCase().split("");
+    for (var index= 0; index < words.length; index +=1) {
+      if (words[index] === "a" || words[index] === "e" || words[index] === "i" || words[index] === "o" || words[index] === "u") {
+        $("#answer").append("-");
+      } else {
+        $("#answer").append(words[index]);
+      }
 
 
 
-    
-  });
+
+
+ }
+});
 });
